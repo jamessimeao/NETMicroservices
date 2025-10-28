25d67c21cf5b1aa8a203013add08f93eb452b901
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-10-28 15:05:56 -0300

    Create IPlatformRepo

---
+++ PlatformService/Data/IPlatformRepo.cs
@@ -0,0 +1,12 @@
+﻿using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public interface IPlatformRepo
+    {
+        public bool SaveChanges();
+        public IEnumerable<PlatformRepo> GetAllPlatforms();
+        public PlatformRepo GetPlatformById(int id);
+        public void CreatePlatform(PlatformRepo platform);
+    }
+}
