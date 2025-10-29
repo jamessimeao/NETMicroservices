3b039f96eb7f8577abad226a621def8373a15038
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-10-29 10:45:56 -0300

    make correction, replace PlatformRepo with Platform

--- PlatformService/Data/IPlatformRepo.cs
+++ PlatformService/Data/IPlatformRepo.cs
@@ -5,8 +5,8 @@ namespace PlatformService.Data
     public interface IPlatformRepo
     {
         public bool SaveChanges();
-        public IEnumerable<PlatformRepo> GetAllPlatforms();
-        public PlatformRepo GetPlatformById(int id);
-        public void CreatePlatform(PlatformRepo platform);
+        public IEnumerable<Platform> GetAllPlatforms();
+        public Platform GetPlatformById(int id);
+        public void CreatePlatform(Platform platform);
     }
 }
