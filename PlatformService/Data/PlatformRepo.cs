70e3d4ddb6a8ae2a0f4e06f6386404a3553d878a
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-10-29 10:55:58 -0300

    Create Platform concrete class

---
+++ PlatformService/Data/PlatformRepo.cs
@@ -0,0 +1,37 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using PlatformService.Models;
+
+namespace PlatformService.Data
+{
+    public class PlatformRepo : IPlatformRepo
+    {
+        private readonly AppDbContext _dbContext;
+
+        public PlatformRepo(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void CreatePlatform(Platform platform)
+        {
+            _dbContext.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetAllPlatforms()
+        {
+            return _dbContext.Platforms.ToList<Platform>();
+        }
+
+        public Platform? GetPlatformById(int id)
+        {
+            return _dbContext.Platforms.FirstOrDefault<Platform>(
+                (Platform platform) => platform.Id == id);
+        }
+
+        public bool SaveChanges()
+        {
+            return (_dbContext.SaveChanges() >= 0);
+        }
+    }
+}
