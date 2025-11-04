b915b2594e4280bd7736e87fa5a45d0f76d2e487
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-04 18:33:16 -0300

    Make DTOs for Platform

---
+++ PlatformService/DTOs/PlatformReadDTO.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PlatformService.DTOs
+{
+    public class PlatformReadDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Publisher { get; set; }
+        public required string Cost { get; set; }
+    }
+}
