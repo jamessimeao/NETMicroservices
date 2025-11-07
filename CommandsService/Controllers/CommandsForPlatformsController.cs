0a6a6805c577b3ee5b9e79ae58dbe0146a4e18a3
jamessimeao <127343204+jamessimeao@users.noreply.github.com>
2025-11-07 19:56:17 -0300

    Create a controller for the Commands service

---
+++ CommandsService/Controllers/CommandsForPlatformsController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsForPlatformsController : ControllerBase
+    {
+        public CommandsForPlatformsController() { }
+
+        [HttpPost]
+        public ActionResult TestInboundConnection()
+        {
+            Console.WriteLine("--> Inbound POST # Command Service");
+            return Ok("Inbound test from Platforms Controller");
+        }
+    }
+}
